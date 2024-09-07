44ea02fae948258a54627dda9c2e1df1ba6e37a6
Dmitriy <3sheepcanfly3@gmail.com>
2024-09-07 14:47:17 +0300

    second commit

--- laboratory_work2.py
+++ laboratory_work2.py
@@ -9,12 +9,31 @@
 # print('Число:')
 # print(square(int(input())))
 
-def max_of_two(x, y):
-    return max(x, y)
+# def max_of_two(x, y):
+#     return max(x, y)
+#
+# print("Числа:")
+# x = int(input())
+# y = int(input())
+#
+#
+# print(f'Наибольшее число: {max_of_two(x, y)}')
 
-print("Числа:")
-x = int(input())
-y = int(input())
+# def describe_person(name, age=30):
+#     print(f'Имя: {name}, возраст: {age}')
+# describe_person('one')
 
 
-print(f'Наибольшее число: {max_of_two(x, y)}')
+print('Число:')
+def is_prime(number):
+    if number <= 1:
+        print('Число не является простым')
+        return
+    for i in range(2, int(number**0.5)+1):
+        if number%i == 0:
+            print('Число не является простым')
+            return
+    print('Число простое')
+is_prime(int(input()))
+
+
--- laboratory_work_1.py
+++ laboratory_work_1.py
@@ -1,7 +1,7 @@
-# print('Введите число: ')
-# a = int(input())
-# for i in range(a):
-#     print(i+1)
+print('Введите число: ')
+a = int(input())
+for i in range(a):
+    print(i+1)
 
 print('Введите первое число: ')
 a = int(input())
